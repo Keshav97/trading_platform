7e93659b4aa826eb3f3bf5cc8ff4032c44f34558
Anirudh <anirudhx5@gmail.com>
2017-03-18 12:13:59 +0000

    cards

--- client/components/Card.js
+++ client/components/Card.js
@@ -16,13 +16,30 @@ export default class Card extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      marketData: {}
+      marketData: {},
+      lastPrice: 0.00,
+      color: "blue-grey"
     };
   }
 
   componentDidMount() {
     this.props.socket.on("onMarketData", (marketData) => {
       if (marketData.symbol === this.props.stockName) {
+        if (marketData.type === "TRADE") {
+          if (marketData.lastPrice > this.state.lastPrice) {
+            this.setState({
+              color: "green"
+            })
+          }
+          else {
+            this.setState({
+              color: "red"
+            })
+          }
+          this.setState({
+            lastPrice: marketData.lastPrice
+          })
+        }
         this.setState({
           marketData: marketData
         })
@@ -32,18 +49,19 @@ export default class Card extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="card blue-grey darken-1">
+        <div className={"card " + this.state.color +  " darken-1"}>
           <div className="card-content white-text">
-            <span class="card-title">{this.props.stockName}</span>
+            <span className="card-title">{this.props.stockName}</span>
+            <h1>{ this.state.lastPrice }</h1>
             <p>Time: { this.state.marketData.time }</p>
             <p>Type: { this.state.marketData.type }</p>
-            <p>Trade: { this.state.marketData.time }</p>
             <p>Bid: { this.state.marketData.bid }</p>
             <p>Ask: { this.state.marketData.ask }</p>
+
+            <button className="waves-effect waves-light btn">Buy</button>
+            <button className="waves-effect waves-light btn">Sell</button>
           </div>
         </div>
-      </div>
     );
   }
 
